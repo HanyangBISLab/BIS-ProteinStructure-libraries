2661a566e775ee08b0449609217abeb76ff2d8b6
HanyangBIS <bislab.hyu@gmail.com>
2024-10-24 15:43:37 +0900

    Update split_pdb.py

--- bis_protein_structure/split_pdb.py
+++ bis_protein_structure/split_pdb.py
@@ -11,25 +11,63 @@ from tqdm import tqdm
 import re
 import numpy as np
 
-res_types = {'ALA' : 'A', 'ARG' : 'R', 'ASN' : 'N', 'ASP' : 'D', 'CYS' : 'C', 'GLN' : 'Q', 'GLU' : 'E', 'GLY' : 'G', 'HIS' : 'H', 'ILE' : 'I', 'LEU' : 'L', \
-           'LYS' : 'K', 'MET' : 'M', 'PHE' : 'F', 'PRO' : 'P', 'SER' : 'S', 'THR' : 'T', 'TRP' : 'W', 'TYR' : 'Y', 'VAL' : 'V', 'UNK' : '-'}
+res_types = {
+    'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C', 'GLN': 'Q', 
+    'GLU': 'E', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 
+    'MET': 'M', 'PHE': 'F', 'PRO': 'P', 'SER': 'S', 'THR': 'T', 'TRP': 'W', 
+    'TYR': 'Y', 'VAL': 'V', 'UNK': '-'
+}
 
 def extract(structure, chain_id, start, end, filename):
-    """Write out selected portion to filename."""
+    """Write out selected portion of a structure to a file.
+
+    Parameters
+    ----------
+    structure : Bio.PDB.Structure
+        The structure object containing the protein data.
+    chain_id : str
+        The identifier for the chain to extract.
+    start : int
+        The starting residue index for extraction.
+    end : int
+        The ending residue index for extraction.
+    filename : str
+        The path to the output file where the selected portion will be saved.
+
+    Returns
+    -------
+    None
+    """
     sel = Dice.ChainSelector(chain_id, start, end)
     io = MMCIFIO()
     io.set_structure(structure)
     io.save(filename, sel)
-    
-    
+
+
 def split_the_pdb(cif_path, pdb, chain):
+    """Split a PDB file into separate CIF files for a specified chain.
+
+    Parameters
+    ----------
+    cif_path : str
+        The path to the input CIF file.
+    pdb : str
+        The name of the PDB structure.
+    chain : str
+        The chain identifier to extract from the PDB structure.
+
+    Returns
+    -------
+    None
+    """
     p = MMCIFParser(QUIET=True)
-    s = p.get_structure(f'{pdb}_{chain}', pdb_path)
+    s = p.get_structure(f'{pdb}_{chain}', cif_path)
     for m in s:
         for cn, c in enumerate(m):
-            if chain != c.id : continue
-                
-            pdb_seq =''
+            if chain != c.id:
+                continue
+
+            pdb_seq = ''
             tmp_res = list()
 
             for i, res in enumerate(c):
